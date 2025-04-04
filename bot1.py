ebbcf80a9d1cec2e1458e0a2c3a616a90589aadb
haotran55 <haot54592@gmail.com>
2025-04-04 12:00:04 +0700

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -322,7 +322,6 @@ def visit_handler(message):
 
     if data.get("status") == "Success":
         info = data["message"]
-            f"<blockquote>\n"
             f"✅ <b>Thành công</b>\n"
             f"🎮 Name: <code>{info[name']}</code>\n"
             f"🆔 Level: <b>{info['level']}</b>\n"
