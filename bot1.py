45bf234b1104980c1313c49e5dae71a7f7beb640
haotran55 <haot54592@gmail.com>
2025-04-06 07:12:18 +0700

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -104,6 +104,7 @@ vietnam_tz = pytz.timezone('Asia/Ho_Chi_Minh')
 ####
 start_time = time.time()
 
+video_url = 'https://v16m-default.akamaized.net/b7650db4ac7f717b7be6bd6a04777a0d/66a418a5/video/tos/useast2a/tos-useast2a-ve-0068-euttp/o4QTIgGIrNbkAPGKKLKteXyLedLE7IEgeSzeE2/?a=0&bti=OTg7QGo5QHM6OjZALTAzYCMvcCMxNDNg&ch=0&cr=0&dr=0&lr=all&cd=0%7C0%7C0%7C0&cv=1&br=2576&bt=1288&cs=0&ds=6&ft=XE5bCqT0majPD12cy-773wUOx5EcMeF~O5&mime_type=video_mp4&qs=0&rc=Mzk1OzY7PGdpZjxkOTQ3M0Bpajh1O2w5cmlzbzMzZjgzM0AuNWJgLi02NjMxLzBgXjUyYSNzNmptMmRjazFgLS1kL2Nzcw%3D%3D&vvpl=1&l=202407261543513F37EAD38E23B6263167&btag=e00088000'
 @bot.message_handler(commands=['add', 'adduser'])
 def add_user(message):
     admin_id = message.from_user.id
@@ -125,9 +126,14 @@ def add_user(message):
     save_user_to_database(connection, user_id, expiration_time)
     connection.close()
 
-    caption_text = (f'<blockquote>NGƯỜI DÙNG CÓ ID {user_id}\nĐÃ ĐƯỢC THÊM VÀO DANH SÁCH VIP\nTHỜI GIAN: {days} DAY\nLỆNH CÓ THỂ SỬ DỤNG CÁC LỆNH TRONG [/start]</blockquote>')
-    load_users_from_database()
-    
+    caption_text = (f'<blockquote>NGƯỜI DÙNG CÓ ID {user_id}\nĐÃ ĐƯỢC THÊM VÀO DANH SÁCH VIP\nTHỜI GIAN: {days} DAY\nLỆNH CÓ THỂ SỬ DỤNG CÁC LỆNH TRONG [/vlong]</blockquote>')
+    bot.send_video(
+        message.chat.id,
+        video_url,
+        caption=caption_text, parse_mode='HTML')
+
+load_users_from_database()
+
 def is_key_approved(chat_id, key):
     if chat_id in users_keys:
         user_key, timestamp = users_keys[chat_id]
@@ -138,6 +144,7 @@ def is_key_approved(chat_id, key):
             else:
                 del users_keys[chat_id]
     return False
+
 
 
 
